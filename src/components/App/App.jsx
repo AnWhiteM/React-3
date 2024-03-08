cbbb5d590045c16a0aee45faa2626f9cd24ed378
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2024-03-08 20:06:05 +0200

    3rd commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,22 +2,27 @@ import { ContactList } from '../ContactList/ContactList'
 import { SearchBox } from '../SearchBox/SearchBox'
 import { useState, useEffect } from 'react';
 import { ContactForm } from '../ContactForm/ContactForm';
+import initialContacts from '../../../initialContacts.json'
 import './App.css'
 
 export const App = () => {
  
   
 
-  const initialContacts  = [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]
+  
 
-  const savedContacts = JSON.parse(localStorage.getItem('contacts')) || initialContacts;
+  const getStoredContacts = () => {
+    const storedContacts = localStorage.getItem('contacts');
+    return storedContacts ? JSON.parse(storedContacts) : initialContacts;
+  };
+  
+  const [contacts, setContacts] = useState(getStoredContacts);
  
-  const [contacts, setContacts] = useState(savedContacts);
+  const [filter, setFilter] = useState('');
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -39,12 +44,17 @@ export const App = () => {
     setContacts(updatedContacts);
   };
   
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  
   return (
     <>
      <h1>Phonebook</h1>
      <ContactForm addContact={addContact} />
-     <SearchBox handleSearch={handleSearch} />
-     <ContactList  contacts={contacts} onDelete={deleteContact}/>
+     <SearchBox handleFilterChange={handleFilterChange} />
+     <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </>
   )
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,22 +1,10 @@
-import { Formik, Form, Field } from "formik"
-export const SearchBox = ({ handleSearch }) => {
-    return(
-        <div>
-        <h2>Search Contacts</h2>
-        <Formik
-          initialValues={{ searchQuery: '' }}
-          onSubmit={(values, { setSubmitting }) => {
-            handleSearch(values.searchQuery);
-            setSubmitting(false);
-          }}
-        >
-          <Form>
-            <Field type="text" name="searchQuery" />
-            <button type="submit">Search</button>
-          </Form>
-        </Formik>
-      </div>
 
-    );
+export const SearchBox = ({ handleFilterChange  }) => {
+  return (
+    <div>
+      <h2>Search Contacts</h2>
+      <input type="text" placeholder="Search by name" onChange={handleFilterChange} />
+    </div>
+  );
 
 }
